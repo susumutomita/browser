d7b0273344bbe2995988bf8f2e231d7b8d0b81db
bull <oyster880@gmail.com>
2024-11-16 07:47:59 +0800

    fix typo (#7)

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -1,21 +1,22 @@
 use alloc::string::String;
 
-#[derive(Debug,Clone,PartialEq)]
-pub struct Url{
-  url: String,
-  host:String,
-  port:String,
-  searchpart:String,
+#[derive(Debug, Clone, PartialEq)]
+pub struct Url {
+    url: String,
+    host: String,
+    port: String,
+    searchpart: String,
 }
 
 impl Url {
-  pub fn new(url: String) -> Self{
-    Self{
-      url,
-      host:"".to_string(),
-      port:"".to_string(),
-      path:"".to_string().
-      searchpart:"".to_string(),
+    pub fn new(url: String) -> Self {
+        Self {
+            url,
+            host: "".to_string(),
+            port: "".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        }
     }
-  }
+    pub fn parse(&mut self) -> Result<Self, String> {}
 }
