08ddc2c3d17fdb88241e1f5dc2c25842ba3c1748
bull <oyster880@gmail.com>
2024-11-18 08:06:42 +0900

    add port

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -56,4 +56,17 @@ impl Url {
             url_parts[0].to_string()
         }
     }
+    fn extract_port(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, '/')
+            .collect();
+
+        if let Some(index) = url_parts[0].find(':') {
+            return url_parts[0][index + 1..].to_string();
+        } else {
+            "80".to_string()
+        }
+    }
 }
