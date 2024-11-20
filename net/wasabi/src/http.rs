4543e0d467fcfae1b8153f5d1109baabc2254d53
bull <oyster880@gmail.com>
2024-11-21 08:54:39 +0900

    add http client

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,9 +1,10 @@
 extern crate alloc;
-use alloc::string::String;
-use alloc::vec::Vec;
-use saba_core::error::Error;
-use saba_core::http::HttpResponse;
+// use alloc::string::String;
+// use alloc::vec::Vec;
+// use saba_core::error::Error;
+// use saba_core::http::HttpResponse;
 
+#[derive(Default)]
 pub struct HttpClient {}
 
 impl HttpClient {
@@ -11,13 +12,13 @@ impl HttpClient {
         Self {}
     }
 
-    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
-        Ok(HttpResponse::new(
-            String::from("HTTP/1.1"),
-            200,
-            String::from("OK"),
-            Vec::new(),
-            String::from(""),
-        ))
-    }
+    // pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+    //     Ok(HttpResponse::new(
+    //         String::from("HTTP/1.1"),
+    //         200,
+    //         String::from("OK"),
+    //         Vec::new(),
+    //         String::from(""),
+    //     ))
+    // }
 }
