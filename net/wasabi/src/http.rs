e90bb41b6d0000524f8d6c851581f80f8a51620a
bull <oyster880@gmail.com>
2024-11-22 09:14:10 +0900

    add host to ip

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,4 +1,7 @@
 extern crate alloc;
+use noli::net::lookup_host;
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
 
 #[derive(Default)]
 pub struct HttpClient {}
@@ -6,5 +9,19 @@ pub struct HttpClient {}
 impl HttpClient {
     pub fn new() -> Self {
         Self {}
+    }
+    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        let ips = match lookup_host(&host) {
+            Ok(ips) => ips,
+            Err(e) => {
+                return Err(Error::Network(format!(
+                    "Failed to find IP Addresses: {:#?} ",
+                    e
+                )))
+            }
+        };
+        if ips.len() < 1 {
+            return Err(Error::Network("No IP Addresses found for".to_string()));
+        }
     }
 }
