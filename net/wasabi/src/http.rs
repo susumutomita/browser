039e5d67e59330b35c3897ea6596d9c976aa4dad
bull <oyster880@gmail.com>
2024-11-22 09:52:13 +0900

    add host to ip

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,5 +1,9 @@
-use crate::alloc::string::ToString;
-use alloc::format;
+extern crate alloc;
+
+use alloc::string::String;
+use alloc::vec::Vec;
+use core::fmt::Write;
+
 use noli::net::lookup_host;
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
@@ -11,18 +15,30 @@ impl HttpClient {
     pub fn new() -> Self {
         Self {}
     }
-    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+
+    pub fn get(&self, host: String, _port: u16, _path: String) -> Result<HttpResponse, Error> {
         let ips = match lookup_host(&host) {
             Ok(ips) => ips,
             Err(e) => {
-                return Err(Error::Network(format!(
-                    "Failed to find IP Addresses: {:#?} ",
-                    e
-                )))
+                // `format!`マクロの代わりに、`String`と`write!`マクロを使用します
+                let mut s = String::from("Failed to find IP Addresses: ");
+                write!(&mut s, "{:#?}", e)
+                    .map_err(|_| Error::Other(String::from("Formatting Error")))?;
+                return Err(Error::Network(s));
             }
         };
-        if ips.len() < 1 {
-            return Err(Error::Network("No IP Addresses found for".to_string()));
+
+        if ips.is_empty() {
+            return Err(Error::Network(String::from("No IP Addresses found for")));
         }
+
+        // 仮のHTTPレスポンスを返します
+        Ok(HttpResponse::new(
+            String::from("HTTP/1.1"),
+            200,
+            String::from("OK"),
+            Vec::new(),
+            String::from(""),
+        ))
     }
 }
