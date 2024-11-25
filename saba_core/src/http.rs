8c2a8b67873d9ac67f003fe789e1997e45668366
bull <oyster880@gmail.com>
2024-11-26 07:44:17 +0800

    enhance response (#25)

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -1,34 +1,87 @@
+use crate::alloc::string::ToString;
+use crate::error::Error;
+use alloc::format;
 use alloc::string::String;
 use alloc::vec::Vec;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct HttpResponse {
     pub version: String,
     pub status_code: u32,
     pub reason: String,
-    pub headers: Vec<(String, String)>,
+    pub headers: Vec<Header>,
     pub body: String,
 }
 
 impl HttpResponse {
-    pub fn new(
-        version: String,
-        status_code: u32,
-        reason: String,
-        headers: Vec<(String, String)>,
-        body: String,
-    ) -> Self {
-        Self {
-            version,
-            status_code,
-            reason,
-            headers,
-            body,
+    pub fn version(&self) -> String {
+        self.version.clone()
+    }
+
+    pub fn status_code(&self) -> u32 {
+        self.status_code
+    }
+
+    pub fn reason(&self) -> String {
+        self.reason.clone()
+    }
+
+    pub fn headers(&self) -> Vec<Header> {
+        self.headers.clone()
+    }
+
+    pub fn body(&self) -> String {
+        self.body.clone()
+    }
+
+    pub fn header_value(&self, name: &str) -> Result<String, Error> {
+        for h in &self.headers {
+            if h.name == name {
+                return Ok(h.value.clone());
+            }
         }
+        Err(Error::UnexpectedInput(format!(
+            "Header not found: {}",
+            name
+        )))
+    }
+    pub fn new(raw_response: String) -> Result<Self, Error> {
+        let preprocessed_response = raw_response.trim_start().replace("\n\r", "\n");
+        let (status_line, remaining) = match preprocessed_response.split_once('\n') {
+            Some((s, r)) => (s, r),
+            None => {
+                return Err(Error::UnexpectedInput(format!(
+                    "Invalid http response: {}",
+                    preprocessed_response
+                )))
+            }
+        };
+        let (headers, body) = match remaining.split_once("\n\n") {
+            Some((h, b)) => {
+                let mut headers = Vec::new();
+                for header in h.split('\n') {
+                    let splitted_header: Vec<&str> = header.splitn(2, ':').collect();
+                    headers.push(Header::new(
+                        String::from(splitted_header[0].trim()),
+                        String::from(splitted_header[1].trim()),
+                    ));
+                }
+                (headers, b)
+            }
+            None => (Vec::new(), remaining),
+        };
+        let statuses: Vec<&str> = status_line.split(' ').collect();
+        Ok(Self {
+            version: statuses[0].to_string(),
+            status_code: statuses[1].parse().unwrap_or(404),
+            reason: statuses[2].to_string(),
+            headers,
+            body: body.to_string(),
+        })
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Header {
     pub name: String,
     pub value: String,
