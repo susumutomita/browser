82c39ca4c39f493e5b6b66d6af55b64264bc7dbe
bull <oyster880@gmail.com>
2024-11-28 08:48:18 +0900

    add html tokenizer

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -1,2 +1,87 @@
-// ファイルの内容をここから開始
-// 余分な空行を削除
+use crate::renderer::html::attribute::Attribute;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+    state: State,
+    pos: usize,
+    reconsume: bool,
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
+}
+
+impl HtmlTokenizer {
+    pub fn new(input: Vec<char>) -> Self {
+        Self {
+            state: State::Data,
+            pos: 0,
+            reconsume: false,
+            latest_token: None,
+            input,
+            buf: String::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    EndTag {
+        tag: String,
+    },
+    Character(char),
+    EOF,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum State {
+    Data,
+    TagOpen,
+    EndTagOpen,
+    TagName,
+    BeforeAttributeName,
+    AttributeName,
+    AfterAttributeName,
+    BeforeAttributeValue,
+    AttributeValueDoubleQuoted,
+    AttributeValueSingleQuoted,
+    AttributeValueUnQuoted,
+    AfterAttributeValueQuoted,
+    SelfClosingStartTag,
+    ScriptData,
+    ScriptDataLessThanSign,
+    ScriptDataEndTagOpen,
+    ScriptDataEndTagName,
+    TemporaryBuffer,
+}
+
+// match self.state {
+//     State::Data => {}
+//     State::TagOpen => {}
+//     State::EndTagOpen => {}
+//     State::TagName => {}
+//     State::BeforeAttributeName => {}
+//     State::AttributeName => {}
+//     State::AfterAttributeName => {}
+// }
+
+impl Iterator for HtmlTokenizer {
+    type Item = HtmlToken;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+        loop {
+            if self.state == State::Data {
+                // Data状態の処理
+            }
+            // 他の状態の処理
+        }
+    }
+}
