f101bcd08ac3400a60a25c0529bdb01d4f9af9a2
bull <oyster880@gmail.com>
2024-11-28 08:48:18 +0900

    add html tokenizer

---
+++ saba_core/src/renderer/html/attribute.rs
@@ -0,0 +1,26 @@
+use alloc::string::String;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Attribute {
+    pub name: String,
+    pub value: String,
+}
+
+impl Attribute {
+    pub fn new(name: String, value: String) -> Self {
+        Self { name, value }
+    }
+    pub fn add_char(&mut self, c: char, is_name: bool) {
+        if is_name {
+            self.name.push(c);
+        } else {
+            self.value.push(c);
+        }
+    }
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+    pub fn value(&self) -> String {
+        self.value.clone()
+    }
+}
