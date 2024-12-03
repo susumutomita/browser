18c50c9411ea584c78e2938311fc06741cc29a4c
bull <oyster880@gmail.com>
2024-12-04 08:58:43 +0900

    add empty token test

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -431,3 +431,16 @@ impl Iterator for HtmlTokenizer {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::alloc::string::ToString;
+
+    #[test]
+    fn test_empty() {
+        let html = "".to_string();
+        let mut tokenizer = HtmlTokenizer::new(html.chars().collect());
+        assert!(tokenizer.next().is_none());
+    }
+}
