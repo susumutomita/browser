eb3dbe6cdc47729b26d90018a70db2354c62a408
bull <oyster880@gmail.com>
2024-12-15 14:12:19 +0900

    modiry main

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@
 
 extern crate alloc;
 use crate::alloc::string::ToString;
-use net_wasabi::http::HttpClient;
-use noli::prelude::*;
 use noli::*;
 use saba_core::browser::Browser;
 use saba_core::http::HttpResponse;
@@ -18,23 +16,22 @@ Data xx xx xx
         <h2 class="class">H2 title</h2>
         <p>Test text.</p>
         <p>
-            <a href="https://www.google.com">Google</a>
-            <a href="https://www.yahoo.com">Yahoo</a>
+            <a href="example.com">Link1</a>
+            <a href="example">Link2</a>
         </p>
     </body>
     </html>
     "#;
 
-fn main() -> u64 {
+fn main() {
     let browser = Browser::new();
     let response =
         HttpResponse::new(TEST_HTTP_RESPONSE.to_string()).expect("failed to parse http response");
-    let page = browser.bollow().current_page();
-    let dom_string = page.borrow_mut().recieve_response(response);
+    let page = browser.borrow().current_page();
+    let dom_string = page.borrow_mut().receive_response(response);
     for log in dom_string.lines() {
         println!("{}", log);
     }
-    0
 }
 
 entry_point!(main);
