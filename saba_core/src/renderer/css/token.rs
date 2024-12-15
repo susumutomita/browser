d9979d71af48dc302cf0eb42ce5607d665805bfd
bull <oyster880@gmail.com>
2024-12-16 08:34:15 +0900

    add css token

--- saba_core/src/renderer/css/token.rs
+++ saba_core/src/renderer/css/token.rs
@@ -1,1 +1,17 @@
-// todo
+use alloc::string::String;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CssToken {
+    HashToken(String),
+    Delim(char),
+    Number(f64),
+    Colon,
+    SemiColon,
+    OpenParenthesis,
+    CloseParenthesis,
+    OpenCurly,
+    CloseCurly,
+    Ident(String),
+    StringToken(String),
+    AtKeyword(String),
+}
