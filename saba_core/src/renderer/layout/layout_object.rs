c0d0c3e2d5c0c73b3d2bd5f9232c10fe2a110a04
bull <oyster880@gmail.com>
2024-12-22 09:14:19 +0900

    add layout point  struct

--- saba_core/src/renderer/layout/layout_object.rs
+++ saba_core/src/renderer/layout/layout_object.rs
@@ -107,3 +107,27 @@ impl LayoutPoint {
         self.y = y;
     }
 }
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct LayoutSize {
+    pub width: i64,
+    pub height: i64,
+}
+
+impl LayoutSize {
+    pub fn new(width: i64, height: i64) -> Self {
+        Self { width, height }
+    }
+    pub fn width(&self) -> i64 {
+        self.width
+    }
+    pub fn height(&self) -> i64 {
+        self.height
+    }
+    pub fn set_width(&mut self, width: i64) {
+        self.width = width;
+    }
+    pub fn set_height(&mut self, height: i64) {
+        self.height = height;
+    }
+}
