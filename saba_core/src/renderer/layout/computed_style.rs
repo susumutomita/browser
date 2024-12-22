412bfa0aacd7fbe3de9da9a6dfdf442913a6ece5
bull <oyster880@gmail.com>
2024-12-22 09:23:15 +0900

    add computed style

--- saba_core/src/renderer/layout/computed_style.rs
+++ saba_core/src/renderer/layout/computed_style.rs
@@ -1,1 +1,24 @@
-// todo
+#[derive(Debug, Clone, PartialEq)]
+pub struct ComputedStyle {
+    background_color: Option<Color>,
+    color: Option<Color>,
+    display: Option<DisplayType>,
+    font_size: Option<i64>,
+    text_decoration: Option<TextDecoration>,
+    height: Option<i64>,
+    width: Option<i64>,
+}
+
+impl ComputedStyle {
+    pub fn new() -> Self {
+        Self {
+            background_color: None,
+            color: None,
+            display: None,
+            font_size: None,
+            text_decoration: None,
+            height: None,
+            width: None,
+        }
+    }
+}
