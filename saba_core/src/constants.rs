10d5f0bf1a36be026de96789f5a2100596fdeba8
bull <oyster880@gmail.com>
2025-01-03 08:59:26 +0900

    add windows constant

--- saba_core/src/constants.rs
+++ saba_core/src/constants.rs
@@ -1,6 +1,8 @@
 pub static WINDOW_WIDTH: i64 = 600;
 pub static WINDOW_HEIGHT: i64 = 400;
 pub static WINDOW_PADDING: i64 = 5;
+pub static WINDOW_INIT_X_POS: i64 = 30;
+pub static WINDOW_INIT_Y_POS: i64 = 50;
 
 pub static TITLE_BAR_HEIGHT: i64 = 24;
 pub static TOOLBAR_HEIGHT: i64 = 26;
@@ -12,3 +14,5 @@ pub static CONTENT_AREA_HEIGHT: i64 =
 pub static CHAR_WIDTH: i64 = 8;
 pub static CHAR_HEIGHT: i64 = 16;
 pub static CHAR_HEIGHT_WITH_PADDING: i64 = CHAR_HEIGHT + 4;
+
+pub static WHITE: u32 = 0xFFFFFF;
