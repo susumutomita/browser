6a15da6588902bc904924f9aac16304be19f35b6
bull <oyster880@gmail.com>
2025-01-03 08:57:04 +0900

    add Wasabi UI (#92)

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,1 +1,11 @@
-// todo
+use alloc::rc::Rc;
+use core::cell::RefCell;
+use noli::window::Window;
+use saba_core::browser::Browser;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct WasabiUI {
+    browser: Rc<RefCell<Browser>>,
+    window: Window,
+}
