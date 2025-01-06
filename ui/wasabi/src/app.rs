fcdbd968047a91297997d3f1636d23e6132e4261
bull <oyster880@gmail.com>
2025-01-06 09:03:51 +0900

    add handle input mode

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,10 +1,12 @@
 use crate::alloc::string::ToString;
 use alloc::format;
 use alloc::rc::Rc;
+use alloc::string::String;
 use core::cell::RefCell;
 use noli::error::Result as OsResult;
 // use noli::prelude::SystemApi;
 // use noli::println;
+use noli::rect::Rect;
 // use noli::sys::api::MouseEvent;
 // use noli::sys::wasabi::Api;
 use noli::window::StringSize;
@@ -24,19 +26,78 @@ use saba_core::constants::WINDOW_INIT_Y_POS;
 use saba_core::constants::WINDOW_WIDTH;
 use saba_core::error::Error;
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[allow(dead_code)]
+enum InputMode {
+    Normal,
+    Editing,
+}
+
 #[derive(Debug)]
 #[allow(dead_code)]
 pub struct WasabiUI {
     browser: Rc<RefCell<Browser>>,
+    input_url: String,
+    input_mode: InputMode,
     window: Window,
 }
 
 #[allow(dead_code)]
 impl WasabiUI {
     fn handle_key_input(&mut self) -> Result<(), Error> {
-        // if let Some(c) = Api::read_key() {
-        //     println!("input text: {:?}", c);
-        // }
+        match self.input_mode {
+            InputMode::Normal => {
+                // let _ = Api::read_key();
+            }
+            InputMode::Editing => {
+                // if let Some(c) = Api::read_key() {
+                //     if c == 0x7F as char || c == 0x08 as char {
+                //         self.input_url.pop();
+                //     } else {
+                //         self.input_url.push(c);
+                //     }
+                // }
+            }
+        }
+        Ok(())
+    }
+
+    fn update_address_bar(&mut self) -> Result<(), Error> {
+        if self
+            .window
+            .fill_rect(WHITE, 72, 4, WINDOW_WIDTH - 76, ADDRESS_BAR_HEIGHT - 2)
+            .is_err()
+        {
+            return Err(Error::InvalidUI(
+                "failed to clear an address bar".to_string(),
+            ));
+        }
+        if self
+            .window
+            .draw_string(
+                BLACK,
+                74,
+                6,
+                &self.input_url,
+                StringSize::Medium,
+                /*underline=*/ false,
+            )
+            .is_err()
+        {
+            return Err(Error::InvalidUI(
+                "failed to clear an address bar".to_string(),
+            ));
+        }
+
+        self.window.flush_area(
+            Rect::new(
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_WIDTH,
+                TOOLBAR_HEIGHT,
+            )
+            .expect("failed to create a rect for the address bar"),
+        );
         Ok(())
     }
     fn handle_mouse_input(&mut self) -> Result<(), Error> {
@@ -113,6 +174,8 @@ impl WasabiUI {
     pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
         Self {
             browser,
+            input_url: String::new(),
+            input_mode: InputMode::Normal,
             window: Window::new(
                 "saba".to_string(),
                 WHITE,
