d3b83c22ee685e09633a117cfb7f4c454a97aa22
bull <oyster880@gmail.com>
2025-01-15 08:30:18 +0900

    pass handle url

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -246,13 +246,13 @@ impl WasabiUI {
         Ok(())
     }
 
-    pub fn start(&mut self) -> Result<(), Error> {
+    pub fn start(&mut self, handle_url: fn(String)) -> Result<(), Error> {
         self.setup()?;
-        self.run_app()?;
+        self.run_app(handle_url)?;
         Ok(())
     }
 
-    fn run_app(&mut self) -> Result<(), Error> {
+    fn run_app(&mut self, handle_url: fn(String)) -> Result<(), Error> {
         loop {
             self.handle_mouse_input()?;
             self.handle_key_input(handle_url)?;
