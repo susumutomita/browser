ca58e5475d6ba08aa0fcbd80c18aa86b34257210
bull <oyster880@gmail.com>
2025-01-25 07:56:54 +0900

    add eval and add sub

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -1,14 +1,115 @@
+use crate::renderer::js::ast::Node;
+use crate::renderer::js::ast::Program;
+use alloc::rc::Rc;
+use core::borrow::Borrow;
+use core::ops::Add;
+use core::ops::Sub;
 #[derive(Debug, Clone)]
 pub struct JsRuntime {}
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum RuntimeValue {
+    Number(u64),
+}
+
+impl Add<RuntimeValue> for RuntimeValue {
+    type Output = RuntimeValue;
+    fn add(self, rhs: RuntimeValue) -> RuntimeValue {
+        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (self, rhs);
+        return RuntimeValue::Number(left_num + right_num);
+    }
+}
+
+impl Sub<RuntimeValue> for RuntimeValue {
+    type Output = RuntimeValue;
+    fn sub(self, rhs: RuntimeValue) -> RuntimeValue {
+        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (self, rhs);
+        return RuntimeValue::Number(left_num - right_num);
+    }
+}
 impl JsRuntime {
     pub fn new() -> Self {
         Self {}
+    }
+    pub fn execute(&mut self, program: &Program) {
+        for node in program.body() {
+            self.eval(&Some(node.clone()));
+        }
+    }
+
+    fn eval(&mut self, node: &Option<Rc<Node>>) -> Option<RuntimeValue> {
+        let node = match node {
+            Some(n) => n,
+            None => return None,
+        };
+
+        match node.borrow() {
+            Node::ExpressionStatement(expr) => return self.eval(&expr),
+            Node::AdditiveExpression {
+                operator,
+                left,
+                right,
+            } => {
+                let left_value = match self.eval(&left) {
+                    Some(value) => value,
+                    None => return None,
+                };
+                let right_value = match self.eval(&right) {
+                    Some(value) => value,
+                    None => return None,
+                };
+
+                if operator == &'+' {
+                    Some(left_value + right_value)
+                } else if operator == &'-' {
+                    Some(left_value - right_value)
+                } else {
+                    None
+                }
+            }
+            Node::AssignmentExpression {
+                operator: _,
+                left: _,
+                right: _,
+            } => None,
+            Node::MemberExpression {
+                object: _,
+                property: _,
+            } => None,
+            Node::NumericLiteral(value) => Some(RuntimeValue::Number(*value)),
+            _ => None,
+        }
     }
 }
 
 impl Default for JsRuntime {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::string::ToString;
+
+    use super::*;
+    use crate::renderer::js::ast::JsParser;
+    use crate::renderer::js::token::JsLexer;
+
+    #[test]
+    fn test_num() {
+        let input = "42".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [Some(RuntimeValue::Number(42))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()));
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
     }
 }
