694103b7a09664f49e57e266dad072244e1556b9
bull <oyster880@gmail.com>
2025-02-04 06:46:22 +0900

    update mamber expression (#144)

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -229,7 +229,31 @@ impl JsRuntime {
                 }
                 None
             }
-            Node::MemberExpression { .. } => None,
+            #[allow(clippy::needless_return)]
+            Node::MemberExpression { object, property } => {
+                let object_value = self.eval(object, env.clone())?;
+                let property_value = match self.eval(property, env.clone()) {
+                    Some(value) => value,
+                    // プロパティが存在しないため、`object_value`をここで返す
+                    None => return Some(object_value),
+                };
+
+                // もしオブジェクトがDOMノードの場合、HtmlElementの`property`を更新する
+                if let RuntimeValue::HtmlElement { object, property } = object_value {
+                    assert!(property.is_none());
+                    // HtmlElementの`property`に`property_value`の文字列をセットする
+                    return Some(RuntimeValue::HtmlElement {
+                        object,
+                        property: Some(property_value.to_string()),
+                    });
+                }
+
+                // document.getElementByIdは、"document.getElementById"という一つの文字列として扱う。
+                // このメソッドへの呼び出しは、"document.getElementById"という名前の関数への呼び出しになる
+                return Some(
+                    object_value + RuntimeValue::StringLiteral(".".to_string()) + property_value,
+                );
+            }
             Node::VariableDeclaration { declarations } => {
                 for declaration in declarations {
                     self.eval(declaration, env.clone());
